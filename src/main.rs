dda1c2126259a77cfd05efe184b4916d9717260a
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-01-03 17:40:14 +0530

    implement from book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use raytracer;
 use std::io;
 use std::io::Write;
 fn main() {
@@ -5,20 +6,20 @@ fn main() {
     const IMAGE_WIDTH: u32 = 256;
     const IMAGE_HEIGHT: i32 = 256;
 
+    // Renderer
+
     print!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n");
     let mut j = IMAGE_HEIGHT - 1;
     while j >= 0 {
         eprint!("\r Scanlines remaining: {j}");
         io::stderr().flush().unwrap();
         for i in 0..IMAGE_WIDTH {
-            let r: f32 = i as f32 / (IMAGE_WIDTH - 1) as f32;
-            let g: f32 = j as f32 / (IMAGE_HEIGHT - 1) as f32;
-            let b = 0.25;
-
-            let r = (255.999 * r) as u32;
-            let g = (255.999 * g) as u32;
-            let b = (255.999 * b) as u32;
-            print!("{r} {g} {b}\n");
+            let pixel_color = raytracer::Vec3::new(
+                (i as f32 / (IMAGE_WIDTH - 1) as f32),
+                (j / IMAGE_HEIGHT) as f32,
+                0.25,
+            );
+            raytracer::write_color!(pixel_color);
         }
         j -= 1;
     }

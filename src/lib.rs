2db29b82efa22a28a11529fe1ea6f70ad3589a46
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-01-04 20:35:02 +0530

    shade sphere using colormap

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,28 @@ use std::io;
 use std::io::Write;
 use vector::Vec3;
 
-fn hit_sphere(center: &Vec3, radius: f32, r: &ray::Ray) -> bool {
+fn hit_sphere(center: &Vec3, radius: f32, r: &ray::Ray) -> f32 {
     let oc = r.origin() - center;
     let a = vector::dot(r.dir(), r.dir());
     let b = 2.0 * vector::dot(&oc, r.dir());
     let c = vector::dot(&oc, &oc) - radius * radius;
     let discriminat = b * b - 4.0 * a * c;
-    discriminat > 0.0
+    if discriminat < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminat.sqrt()) / (2.0 * a);
+    }
 }
 
 fn ray_color(r: &ray::Ray) -> Vec3 {
-    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Vec3::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = vector::unit_vector(&(r.at(t) - Vec3::new(0.0, 0.0, -1.0)));
+        return Vec3::new(n['x'] + 1.0, n['y'] + 1.0, n['z'] + 1.0) * 0.5;
     }
     let unit_direction = vector::unit_vector(r.dir());
     let t = 0.5 * (unit_direction['y'] + 1.0);
-    Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + vector::Vec3::new(0.5, 0.7, 1.0) * t
+    return Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + vector::Vec3::new(0.5, 0.7, 1.0) * t;
 }
 
 pub fn render() {

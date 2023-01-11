efaa45def01f44213b53498179557bd6ac91d488
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-01-11 14:23:14 +0530

    correct sphere collision

--- src/sphere.rs
+++ src/sphere.rs
@@ -21,30 +21,26 @@ impl Hittable for Sphere {
     fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
         let oc = r.origin() - &self.center;
         let a = r.dir().length_squared();
-        let half_b = vector::dot(&oc, r.dir());
+        let half_b = vector::dot(&oc, &r.dir());
         let c = oc.length_squared() - self.radius * self.radius;
-        let discriminat = half_b * half_b - a * c;
-        let sqrtd = discriminat.sqrt();
+        let discriminant = half_b * half_b - a * c;
 
-        // finding the nearest root that lies in the acceptable range
-        let mut root = (-half_b - sqrtd) / a;
-        if root < t_min || t_max < root {
-            root = (-half_b - sqrtd) / a;
-            if root < t_min || t_max < root {
-                return None;
+        if discriminant > 0.0 {
+            let root = discriminant.sqrt();
+            let temp = (-half_b - root) / a;
+            if temp < t_max && temp > t_min {
+                let p = r.at(temp);
+                let normal = (&p - &self.center) / self.radius;
+                let front_face = vector::dot(r.dir(), &normal) < 0.0;
+
+                return Some(HitRecord {
+                    t: temp,
+                    p,
+                    normal: if front_face { normal } else { normal * -1.0 },
+                    front_face,
+                });
             }
         }
-
-        let p = r.at(root);
-        let normal = (&p - &self.center) / self.radius;
-        let outward_normal = (&p - &self.center) / self.radius;
-        let front_face = vector::dot(r.dir(), &outward_normal) < 0.0;
-
-        Some(HitRecord {
-            p,
-            normal: if front_face { normal } else { normal * -1.0 },
-            t: root,
-            front_face,
-        })
+        None
     }
 }

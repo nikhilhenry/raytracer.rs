6b1d8f7dc037f414ee80d9d4ae202ff42cf7c033
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-06-28 16:28:46 +0530

    add metal spheres

--- src/lib.rs
+++ src/lib.rs
@@ -54,19 +54,34 @@ pub fn render() {
     let material_center = rc::Rc::new(material::Lambertian {
         albedo: Vec3::new(0.7, 0.3, 0.0),
     });
+    let material_left = rc::Rc::new(material::Metal {
+        albedo: Vec3::new(0.8, 0.8, 0.8),
+    });
+    let material_right = rc::Rc::new(material::Metal {
+        albedo: Vec3::new(0.8, 0.6, 0.2),
+    });
     // World
     let mut world = hittable::HittableList::new();
+    world.add(rc::Rc::new(Sphere::new(
+        Vec3::new(0.0, -100.5, -1.0),
+        100.0,
+        material_ground,
+    )));
     world.add(rc::Rc::new(Sphere::new(
         Vec3::new(0.0, 0.0, -1.0),
         0.5,
         material_center,
     )));
     world.add(rc::Rc::new(Sphere::new(
-        Vec3::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
+        Vec3::new(-1.0, 0.0, -1.0),
+        0.5,
+        material_left,
     )));
-
+    world.add(rc::Rc::new(Sphere::new(
+        Vec3::new(1.0, 0.0, -1.0),
+        0.5,
+        material_right,
+    )));
     // Camera
     let cam = Camera::new();
     // Renderer
